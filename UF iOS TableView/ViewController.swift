3ba950680ba9057405c6402f5daa485908e7dae5
Siddha Tiwari <siddhatiwari@gmail.com>
2018-11-15 17:15:45 -0500

    Initial Commit

---
+++ UF iOS TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UF iOS TableView
+//
+//  Created by Siddha Tiwari on 11/15/18.
+//  Copyright © 2018 Siddha Tiwari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
